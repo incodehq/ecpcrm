522575b1170dc0654a9f4d7b76bf5523ab8ff409
Rosco Kalis <roscokalis@gmail.com>
2016-10-06 11:27:38 +0200

    ECPCRM-33: Added a general EcpCrmTest in the webapp directory, which includes the general tests, as well as the functions needed for other tests

---
+++ webapp/src/test/java/org/incode/eurocommercial/ecpcrm/webapp/card_check_unbound/CardCheckUnboundTest.java
@@ -0,0 +1,40 @@
+package org.incode.eurocommercial.ecpcrm.webapp.card_check_unbound;
+
+import org.approvaltests.Approvals;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.incode.eurocommercial.ecpcrm.webapp.ecp_crm_test.EcpCrmTest;
+
+public class CardCheckUnboundTest extends EcpCrmTest {
+    @Test
+    public void when_card_does_not_exist_and_has_invalid_number_we_expect_312_error() throws Exception {
+        Approvals.verifyJson(sendRequest("CardCheckUnboundTest.when_card_does_not_exist_and_has_invalid_number_we_expect_312_error.json"));
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_is_not_enabled_we_expect_303_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_is_not_the_same_center_as_device_we_expect_317_error() throws Exception {
+    }
+
+    @Test
+    public void when_card_exists_but_is_already_bound_to_user_we_expect_308_error() throws Exception {
+        Approvals.verifyJson(sendRequest("CardCheckUnboundTest.when_card_exists_but_is_already_bound_to_user_we_expect_308_error.json"));
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_and_is_unbound_we_expect_happy_response_existing_card() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_does_not_exist_but_has_valid_number_we_expect_happy_response_non_existing_card() throws Exception {
+    }
+
+}
---
+++ webapp/src/test/java/org/incode/eurocommercial/ecpcrm/webapp/ecp_crm_test/EcpCrmTest.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2015-2016 Eurocommercial Properties NV
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.incode.eurocommercial.ecpcrm.webapp.ecp_crm_test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.github.mjeanroy.junit.servers.jetty.EmbeddedJetty;
+import com.github.mjeanroy.junit.servers.rules.JettyServerRule;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Resources;
+
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.macosx.P4MergeReporter;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.incode.eurocommercial.ecpcrm.webapp.card_check_unbound.CardCheckUnboundTest;
+
+//@UseReporter(MyBeyondCompare3Reporter.class)
+@UseReporter(P4MergeReporter.class)
+public abstract class EcpCrmTest {
+
+    //    static {
+    //        System.setProperty("isis.appManifest", "...");
+    //    }
+
+    @ClassRule
+    public static JettyServerRule server = new JettyServerRule(new EmbeddedJetty());
+    private GenericUrl url = new GenericUrl(server.getUrl() + "restful/crm/api/6.0/card-check-unbound");
+
+    private final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    private HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+        @Override public void initialize(final HttpRequest httpRequest) throws IOException {
+            httpRequest.setHeaders(new HttpHeaders() {
+                {
+                    setBasicAuthentication("ecpcrm-admin","pass");
+                }
+            });
+        }
+    });
+
+    protected String sendRequest(String resourceFile) throws Exception {
+        final URL resource = Resources.getResource(CardCheckUnboundTest.class, resourceFile);
+        final String json = Resources.toString(resource, Charsets.UTF_8);
+
+        HttpContent content = ByteArrayContent.fromString("application/json", json);
+        HttpRequest request = requestFactory.buildPostRequest(url, content);
+        HttpResponse response = request.execute();
+
+        return CharStreams.toString(new InputStreamReader(response.getContent()));
+    }
+
+    @Test
+    @Ignore
+    public void when_request_is_invalid_we_expect_300_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_request_is_unauthenticated_we_expect_310_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_device_is_invalid_we_expect_301_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_required_parameter_is_missing_we_expect_302_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_parameter_is_invalid_we_expect_302_error() throws Exception {
+    }
+}
