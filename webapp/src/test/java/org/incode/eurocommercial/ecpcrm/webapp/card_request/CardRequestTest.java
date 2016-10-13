19e3d835bfe2e51e2edb5c82db5a3a8d738c835a
Rosco Kalis <roscokalis@gmail.com>
2016-10-13 11:57:40 +0200

    Moved all test stubs for all endpoints to the new webapp folder

---
+++ webapp/src/test/java/org/incode/eurocommercial/ecpcrm/webapp/card_request/CardRequestTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2015-2016 Eurocommercial Properties NV
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.incode.eurocommercial.ecpcrm.webapp.card_request;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.incode.eurocommercial.ecpcrm.webapp.ecp_crm_test.EcpCrmTest;
+
+public class CardRequestTest extends EcpCrmTest {
+    @Test
+    @Ignore
+    public void when_email_exists_and_user_is_invalid_we_expect_304_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_and_check_is_valid_and_XXX_we_expect_318_error() throws Exception { // I don't understand 318
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_and_check_is_invalid_we_expect_306_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_and_user_does_not_match_we_expect_305_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_and_no_check_is_made_we_expect_318_error() throws Exception { // I don't understand 318
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_and_card_is_already_bound_we_expect_card_to_be_tagged_as_lost() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_email_exists_but_card_already_has_a_handled_request_we_expect_307_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_user_data_cant_be_updated_we_expect_316_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_user_data_can_be_updated_we_expect_happy_response() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_no_request_already_exists_we_expect_it_to_be_created() throws Exception {
+    }
+
+
+}
---
+++ webapp/src/test/java/org/incode/eurocommercial/ecpcrm/webapp/user_create/UserCreateTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2015-2016 Eurocommercial Properties NV
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.incode.eurocommercial.ecpcrm.webapp.user_create;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.incode.eurocommercial.ecpcrm.webapp.ecp_crm_test.EcpCrmTest;
+
+public class UserCreateTest extends EcpCrmTest {
+    @Test
+    @Ignore
+    public void when_card_does_not_exist_and_has_invalid_number_we_expect_312_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_is_not_enabled_we_expect_303_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_is_not_the_same_center_as_device_we_expect_317_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_is_already_bound_to_user_we_expect_308_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_email_already_exists_we_expect_309_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_new_user_is_created_but_its_data_cant_be_updated_we_expect_316_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_exists_but_cant_be_bound_we_expect_314_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_does_not_exist_and_cant_be_created_or_bound_we_expect_314_error() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void when_card_can_be_bound_we_expect_happy_response() throws Exception {
+    }
+
+}
