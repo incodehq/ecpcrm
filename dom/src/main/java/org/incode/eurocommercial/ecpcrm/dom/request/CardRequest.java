f1a464f234accca6ffb370eca1eba764166de432
Rosco Kalis <roscokalis@gmail.com>
2016-12-30 15:31:58 +0100

    ECPCRM-61, ECPCRM-69, ECPCRM-70: Added the CardRequest DO with a repository, menu and layout file

---
+++ dom/src/main/java/org/incode/eurocommercial/ecpcrm/dom/request/CardRequest.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2015-2016 Eurocommercial Properties NV
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.incode.eurocommercial.ecpcrm.dom.request;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.Queries;
+import javax.jdo.annotations.Query;
+
+import org.joda.time.LocalDate;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.PropertyLayout;
+import org.apache.isis.applib.annotation.Where;
+
+import org.incode.eurocommercial.ecpcrm.dom.card.Card;
+import org.incode.eurocommercial.ecpcrm.dom.user.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@PersistenceCapable(
+        identityType = IdentityType.DATASTORE
+)
+@Queries({
+        @Query(
+                name = "findByApproved", language = "JDOQL",
+                value = "SELECT "
+                        + "FROM org.incode.eurocommercial.ecpcrm.dom.request.CardRequest "
+                        + "WHERE approved == :approved "),
+        @Query(
+                name = "findByApprovedAndUser", language = "JDOQL",
+                value = "SELECT "
+                        + "FROM org.incode.eurocommercial.ecpcrm.dom.request.CardRequest "
+                        + "WHERE approved == :approved "
+                        + "&& requestingUser == :requestingUser ")
+})
+@DomainObject(
+        editing = Editing.DISABLED
+)
+public class CardRequest {
+    public String title() {
+        return requestingUser.getFirstName() + " " + requestingUser.getLastName() + " - " + date.toString();
+    }
+
+    @Column(allowsNull = "false")
+    @Property
+    @Getter @Setter
+    private User requestingUser;
+
+    @Column(allowsNull = "false")
+    @Property
+    @Persistent
+    @Getter @Setter
+    private LocalDate date;
+
+    @Column(allowsNull = "true")
+    @Property
+    @PropertyLayout(hidden = Where.ALL_TABLES)
+    @Getter @Setter
+    private Card assignedCard;
+
+    @Column(allowsNull = "true")
+    @Property
+    @PropertyLayout(hidden = Where.ALL_TABLES)
+    @Getter @Setter
+    private Boolean approved;
+
+    @Action
+    public CardRequest approve(String cardNumber) {
+        requestingUser.giveCard(cardNumber);
+        setApproved(true);
+        return this;
+    }
+
+    public String validateApprove(String cardNumber) {
+        return requestingUser.validateGiveCard(cardNumber);
+    }
+
+    @Action
+    public CardRequest deny() {
+        setApproved(false);
+        return this;
+    }
+
+    public boolean hideApprove() {
+        return getApproved() != null;
+    }
+
+    public boolean hideDeny() {
+        return getApproved() != null;
+    }
+
+    public boolean hideAssignedCard() {
+        return assignedCard == null;
+    }
+
+    public boolean hideApproved() {
+        return approved == null;
+    }
+
+}
---
+++ dom/src/main/java/org/incode/eurocommercial/ecpcrm/dom/request/CardRequestMenu.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2015-2016 Eurocommercial Properties NV
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.incode.eurocommercial.ecpcrm.dom.request;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY
+)
+@DomainServiceLayout(
+        named = "Card Requests"
+)
+public class CardRequestMenu {
+    @Action(semantics = SemanticsOf.SAFE)
+    public List<CardRequest> allOpenRequests() {
+        return cardRequestRepository.listOpenRequests();
+    }
+
+    @Inject CardRequestRepository cardRequestRepository;
+}
