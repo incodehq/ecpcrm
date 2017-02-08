0f910cea7da4eed538933099d39068d93841adf4
Rosco Kalis <roscokalis@gmail.com>
2017-02-08 16:53:16 +0100

    ECPCRM-44 and ECPCRM-45: Added most of the user-update and -create endpoints, however, the center shenanigans needs to be sorted out

--- webapp/src/main/java/org/incode/eurocommercial/ecpcrm/restapi/EcpCrmResource.java
+++ webapp/src/main/java/org/incode/eurocommercial/ecpcrm/restapi/EcpCrmResource.java
@@ -16,6 +16,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import org.assertj.core.util.Strings;
 import org.joda.time.LocalDate;
 
 import org.apache.isis.applib.annotation.Where;
@@ -26,6 +27,11 @@ import org.apache.isis.viewer.restfulobjects.rendering.service.RepresentationSer
 import org.apache.isis.viewer.restfulobjects.rendering.service.conneg.PrettyPrinting;
 import org.apache.isis.viewer.restfulobjects.server.resources.ResourceAbstract;
 
+import org.incode.eurocommercial.ecpcrm.dom.CardStatus;
+import org.incode.eurocommercial.ecpcrm.dom.Title;
+import org.incode.eurocommercial.ecpcrm.dom.card.Card;
+import org.incode.eurocommercial.ecpcrm.dom.card.CardRepository;
+import org.incode.eurocommercial.ecpcrm.dom.center.CenterRepository;
 import org.incode.eurocommercial.ecpcrm.dom.user.User;
 import org.incode.eurocommercial.ecpcrm.dom.user.UserRepository;
 
@@ -156,14 +162,114 @@ public class EcpCrmResource extends ResourceAbstract  {
             MediaType.APPLICATION_XML, RestfulMediaType.APPLICATION_XML_OBJECT, RestfulMediaType.APPLICATION_XML_ERROR
     })
     @PrettyPrinting
-    public Response userCreate(InputStream body) {
+    public Response userCreate(
+            @HeaderParam("card") String cardNumber,
+            @HeaderParam("title") String title,
+            @HeaderParam("first_name") String firstName,
+            @HeaderParam("last_name") String lastName,
+            @HeaderParam("address") String address,
+            @HeaderParam("zipcode") String zipcode,
+            @HeaderParam("city") String city,
+            @HeaderParam("email") String email,
+            @HeaderParam("optin") int promotionalEmails
+            ) {
         init(RepresentationType.DOMAIN_OBJECT, Where.OBJECT_FORMS, RepresentationService.Intent.ALREADY_PERSISTENT);
 
+        if(title == null || Strings.isNullOrEmpty(firstName) || Strings.isNullOrEmpty(lastName)) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 302)
+                            .add("message", "Invalid parameter")
+                            .toJsonString())
+                    .build();
+        }
+
+        if(!cardRepository.cardNumberIsValid(cardNumber)) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 312)
+                            .add("message", "Invalid card number")
+                            .toJsonString())
+                    .build();
+        }
+
+        Card card = cardRepository.findByExactNumber(cardNumber);
+
+        if(card != null) {
+            if(card.getStatus() != CardStatus.ENABLED) {
+                return Response
+                        .ok()
+                        .type(MediaType.APPLICATION_JSON_TYPE)
+                        .entity(new JsonBuilder()
+                                .add("status", 303)
+                                .add("message", "Invalid card")
+                                .toJsonString())
+                        .build();
+            }
+
+            //TODO: Check against center of hostess
+
+            if(card.getOwner() != null) {
+                return Response
+                        .ok()
+                        .type(MediaType.APPLICATION_JSON_TYPE)
+                        .entity(new JsonBuilder()
+                                .add("status", 308)
+                                .add("message", "Card is already bound to another user")
+                                .toJsonString())
+                        .build();
+            }
+        }
+
+        User user = userRepository.findByExactEmail(email);
+        if(user != null) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 309)
+                            .add("message", "Email already exists in our system")
+                            .toJsonString())
+                    .build();
+        }
+
+        //TODO: Extract center of request
+        user = userRepository.findOrCreate(true, Title.valueOf(title), firstName, lastName, email,
+                                           address, zipcode, city, null, null, cardNumber,
+                                           asBoolean(promotionalEmails), null, null);
+        if(user == null) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 316)
+                            .add("message", "Failed to create or update user")
+                            .toJsonString())
+                    .build();
+        }
+        card = cardRepository.findByExactNumber(cardNumber);
+
+        if(card != null && card.getOwner() != user) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 314)
+                            .add("message", "Failed to bind user to card")
+                            .toJsonString())
+                    .build();
+        }
 
         return Response
                 .ok()
                 .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(String.format("{ \"status\": 200, \"message\": \"test\"}"))
+                .entity(new JsonBuilder()
+                        .add("status", 200)
+                        .toJsonString())
                 .build();
     }
 
@@ -175,13 +281,125 @@ public class EcpCrmResource extends ResourceAbstract  {
             MediaType.APPLICATION_XML, RestfulMediaType.APPLICATION_XML_OBJECT, RestfulMediaType.APPLICATION_XML_ERROR
     })
     @PrettyPrinting
-    public Response userUpdate(InputStream body) {
+    public Response userUpdate(
+            @HeaderParam("card") String cardNumber,
+            @HeaderParam("title") String title,
+            @HeaderParam("first_name") String firstName,
+            @HeaderParam("last_name") String lastName,
+            @HeaderParam("address") String address,
+            @HeaderParam("zipcode") String zipcode,
+            @HeaderParam("city") String city,
+            @HeaderParam("email") String email,
+            @HeaderParam("optin") int promotionalEmails
+    ) {
         init(RepresentationType.DOMAIN_OBJECT, Where.OBJECT_FORMS, RepresentationService.Intent.ALREADY_PERSISTENT);
+
+        if(title == null || Strings.isNullOrEmpty(firstName) || Strings.isNullOrEmpty(lastName)
+                || Strings.isNullOrEmpty(cardNumber) || Strings.isNullOrEmpty(email)) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 302)
+                            .add("message", "Invalid parameter")
+                            .toJsonString())
+                    .build();
+        }
+
+        if(!cardRepository.cardNumberIsValid(cardNumber)) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 312)
+                            .add("message", "Invalid card number")
+                            .toJsonString())
+                    .build();
+        }
+
+        Card card = cardRepository.findByExactNumber(cardNumber);
+
+        if(card != null) {
+            if(card.getStatus() != CardStatus.ENABLED) {
+                return Response
+                        .ok()
+                        .type(MediaType.APPLICATION_JSON_TYPE)
+                        .entity(new JsonBuilder()
+                                .add("status", 303)
+                                .add("message", "Invalid card")
+                                .toJsonString())
+                        .build();
+            }
+
+            //TODO: Check against center of hostess
+
+            if(card.getOwner() != null) {
+                return Response
+                        .ok()
+                        .type(MediaType.APPLICATION_JSON_TYPE)
+                        .entity(new JsonBuilder()
+                                .add("status", 308)
+                                .add("message", "Card is already bound to another user")
+                                .toJsonString())
+                        .build();
+            }
+        }
+
+        User user = userRepository.findByExactEmail(email);
+        if(user != null && !(firstName.equals(user.getFirstName()) && lastName.equals(user.getLastName()))) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 309)
+                            .add("message", "Email already exists in our system")
+                            .toJsonString())
+                    .build();
+        }
+
+        //TODO: Extract center of request
+        user = userRepository.findOrCreate(true, Title.valueOf(title), firstName, lastName, email,
+                address, zipcode, city, null, null, cardNumber,
+                asBoolean(promotionalEmails), null, null);
+        if(user == null) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 316)
+                            .add("message", "Failed to create or update user")
+                            .toJsonString())
+                    .build();
+        }
+        user.setTitle(Title.valueOf(title));
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setAddress(address);
+        user.setZipcode(zipcode);
+        user.setCity(city);
+        user.setPromotionalEmails(asBoolean(promotionalEmails));
+        user.newCard(cardNumber);
+
+        card = cardRepository.findByExactNumber(cardNumber);
+
+        if(card != null && card.getOwner() != user) {
+            return Response
+                    .ok()
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(new JsonBuilder()
+                            .add("status", 314)
+                            .add("message", "Failed to bind user to card")
+                            .toJsonString())
+                    .build();
+        }
 
         return Response
                 .ok()
                 .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(String.format("{ \"status\": 200, \"message\": \"test\"}"))
+                .entity(new JsonBuilder()
+                        .add("status", 200)
+                        .toJsonString())
                 .build();
     }
 
@@ -234,6 +452,9 @@ public class EcpCrmResource extends ResourceAbstract  {
                 .build();
     }
 
+    public static boolean asBoolean(final int i) {
+        return i > 0;
+    }
     public static String asString(final int i) {
         return "" + i;
     }
@@ -283,5 +504,7 @@ public class EcpCrmResource extends ResourceAbstract  {
     }
 
     @Inject UserRepository userRepository;
+    @Inject CardRepository cardRepository;
+    @Inject CenterRepository centerRepository;
     @Inject ClockService clockService;
 }
