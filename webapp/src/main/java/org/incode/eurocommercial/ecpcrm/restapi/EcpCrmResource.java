edde0392cdb5fca51825281064da94b8b329cc8c
Rosco Kalis <roscokalis@gmail.com>
2017-02-15 15:19:19 +0100

    Removed outdated endpoints

--- webapp/src/main/java/org/incode/eurocommercial/ecpcrm/restapi/EcpCrmResource.java
+++ webapp/src/main/java/org/incode/eurocommercial/ecpcrm/restapi/EcpCrmResource.java
@@ -151,60 +151,6 @@ public class EcpCrmResource extends ResourceAbstract  {
     }
 
     @POST
-    @Path("/card-request-detail")
-    @Consumes({ MediaType.APPLICATION_JSON })
-    @Produces({
-            MediaType.APPLICATION_JSON, RestfulMediaType.APPLICATION_JSON_OBJECT, RestfulMediaType.APPLICATION_JSON_ERROR,
-            MediaType.APPLICATION_XML, RestfulMediaType.APPLICATION_XML_OBJECT, RestfulMediaType.APPLICATION_XML_ERROR
-    })
-    @PrettyPrinting
-    public Response cardRequestDetail(InputStream body) {
-        init(RepresentationType.DOMAIN_OBJECT, Where.OBJECT_FORMS, RepresentationService.Intent.ALREADY_PERSISTENT);
-
-        return Response
-                .ok()
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(String.format("{ \"status\": 200, \"message\": \"test\"}"))
-                .build();
-    }
-
-    @POST
-    @Path("/card-request-edit")
-    @Consumes({ MediaType.APPLICATION_JSON })
-    @Produces({
-            MediaType.APPLICATION_JSON, RestfulMediaType.APPLICATION_JSON_OBJECT, RestfulMediaType.APPLICATION_JSON_ERROR,
-            MediaType.APPLICATION_XML, RestfulMediaType.APPLICATION_XML_OBJECT, RestfulMediaType.APPLICATION_XML_ERROR
-    })
-    @PrettyPrinting
-    public Response cardRequestEdit(InputStream body) {
-        init(RepresentationType.DOMAIN_OBJECT, Where.OBJECT_FORMS, RepresentationService.Intent.ALREADY_PERSISTENT);
-
-        return Response
-                .ok()
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(String.format("{ \"status\": 200, \"message\": \"test\"}"))
-                .build();
-    }
-
-    @POST
-    @Path("/card-requests")
-    @Consumes({ MediaType.APPLICATION_JSON })
-    @Produces({
-            MediaType.APPLICATION_JSON, RestfulMediaType.APPLICATION_JSON_OBJECT, RestfulMediaType.APPLICATION_JSON_ERROR,
-            MediaType.APPLICATION_XML, RestfulMediaType.APPLICATION_XML_OBJECT, RestfulMediaType.APPLICATION_XML_ERROR
-    })
-    @PrettyPrinting
-    public Response cardRequests(InputStream body) {
-        init(RepresentationType.DOMAIN_OBJECT, Where.OBJECT_FORMS, RepresentationService.Intent.ALREADY_PERSISTENT);
-
-        return Response
-                .ok()
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(String.format("{ \"status\": 200, \"message\": \"test\"}"))
-                .build();
-    }
-
-    @POST
     @Path("/user-create")
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({
