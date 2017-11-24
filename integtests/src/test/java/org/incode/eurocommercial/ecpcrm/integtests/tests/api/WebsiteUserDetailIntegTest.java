220e086a135b807e274d356b17dc0450e8e1bc59
Rosco Kalis <roscokalis@gmail.com>
2017-11-24 16:58:21 +0100

    ECPCRM-57, ECPCRM-132: Added missing test stubs for website-user-detail

--- integtests/src/test/java/org/incode/eurocommercial/ecpcrm/integtests/tests/api/WebsiteUserDetailIntegTest.java
+++ integtests/src/test/java/org/incode/eurocommercial/ecpcrm/integtests/tests/api/WebsiteUserDetailIntegTest.java
@@ -31,6 +31,7 @@ import org.apache.isis.applib.services.clock.ClockService;
 
 import org.incode.eurocommercial.ecpcrm.app.services.api.ApiService;
 import org.incode.eurocommercial.ecpcrm.app.services.api.Result;
+import org.incode.eurocommercial.ecpcrm.app.services.api.UserViewModel;
 import org.incode.eurocommercial.ecpcrm.dom.card.CardRepository;
 import org.incode.eurocommercial.ecpcrm.dom.center.Center;
 import org.incode.eurocommercial.ecpcrm.dom.numerator.NumeratorRepository;
@@ -76,7 +77,7 @@ public class WebsiteUserDetailIntegTest extends EcpCrmIntegTest {
     }
 
     @Test
-    public void when_required_parameter_is_missing_we_expect_302_error() throws Exception {
+    public void when_required_parameter_is_missing_we_expect_403_error() throws Exception {
         // given
         Center center = this.center;
         String email = user.getEmail();
@@ -95,7 +96,54 @@ public class WebsiteUserDetailIntegTest extends EcpCrmIntegTest {
             Result result = (Result) m.invoke(apiService, a);
 
             // then
-            assertThat(result.getStatus()).isEqualTo(302);
+            assertThat(result.getStatus()).isEqualTo(403);
         }
+    }
+
+    @Test
+    public void when_user_does_not_exist_we_expect_304_error() throws Exception {
+        // given
+        Center center = this.center;
+        String email = "THIS IS DEFINITELY NOT AN EXISTING EMAIL";
+        String checkCode = apiService.computeCheckCode(email);
+
+        // when
+        Result result = apiService.websiteUserDetail(center, email, checkCode);
+
+        // then
+        assertThat(result.getStatus()).isEqualTo(304);
+    }
+
+    @Test
+    public void when_check_code_is_incorrect_we_expect_402_error() throws Exception {
+        // given
+        Center center = this.center;
+        String email = user.getEmail();
+        String checkCode = "INCORRECT CHECK CODE";
+
+        // when
+        Result result = apiService.websiteUserDetail(center, email, checkCode);
+
+        // then
+        assertThat(result.getStatus()).isEqualTo(402);
+    }
+
+    @Test
+    public void when_user_exists_and_check_code_is_correct_we_expect_happy_case() throws Exception {
+        // given
+        Center center = this.center;
+        String email = user.getEmail();
+        String checkCode = apiService.computeCheckCode(email);
+
+        // when
+        Result result = apiService.websiteUserDetail(center, email, checkCode);
+
+        // then
+        assertThat(result.getStatus()).isEqualTo(200);
+        assertThat(result.getResponse() instanceof UserViewModel);
+
+        UserViewModel response = (UserViewModel) result.getResponse();
+        assertThat(response.getId()).isEqualTo(user.getReference());
+        assertThat(response).isEqualTo(UserViewModel.fromUser(user));
     }
 }
