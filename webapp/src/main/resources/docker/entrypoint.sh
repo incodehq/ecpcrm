8502521fbbdf4527ce95539644e7be36686ac96b
Rosco Kalis <roscokalis@gmail.com>
2018-08-16 13:16:30 +0200

    Added entrypoint script and updated dockerfile

---
+++ webapp/src/main/resources/docker/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+echo "CATALINA_HOME = " $CATALINA_HOME
+
+mkdir -p /run/conf
+
+if [ -s /run/secrets/*.context.xml ];
+  then
+    # Symlink context.xml.
+    ln -sf /run/secrets/*.context.xml $CATALINA_HOME/conf/Catalina/localhost/ROOT.xml
+    echo "FOUND context.xml."
+  else
+    echo "context.xml NOT FOUND, proceeding with default config"
+fi
+
+if [ -s /run/secrets/*.shiro.ini ];
+  then
+    # Symlink shiro.ini.
+    ln -sf /run/secrets/*.shiro.ini /run/conf/shiro.ini
+    echo "FOUND shiro.ini."
+  else
+    echo "shiro.ini NOT FOUND, proceeding with default config"
+fi
+
+if [ -s /run/secrets/*.spring.properties ];
+  then
+    # Symlink spring.properties.
+    ln -sf /run/secrets/*.spring.properties /run/conf/spring.properties
+    echo "FOUND spring.properties."
+  else
+    echo "spring.properties NOT FOUND, proceeding with default config"
+fi
+
+if [ -s /run/secrets/*.setenv.sh ];
+  then
+    # Symlink setenv.sh.
+    ln -sf /run/secrets/*.setenv.sh $CATALINA_HOME/bin/setenv.sh
+    echo "FOUND setenv.sh."
+  else
+    echo "setenv.sh NOT FOUND, proceeding with default config"
+fi
+
+if [ -s /run/secrets/*.isis.properties ] && [ -s /run/secrets/*.viewer_wicket.properties ] && [ -s /run/secrets/*.logging.properties ];
+  then
+    # Symlink isis.properties.
+    ln -sf /run/secrets/*.isis.properties /run/conf/isis.properties
+    ln -sf /run/secrets/*.viewer_wicket.properties /run/conf/viewer_wicket.properties
+    ln -sf /run/secrets/*.logging.properties /run/conf/logging.properties
+    echo "FOUND isis.properties and viewer_wicket.properties and logging.properties."
+  else
+    echo "isis.properties and/or viewer_wicket.properties and/or logging.properties NOT FOUND, proceeding with default config"
+fi
+
+# Running Catalina
+echo "Starting Catalina:"
+${SERVER_HOME}/bin/catalina.sh run
