e73cd4599959f7cd36f6e0eb4ea0f6cce0934e7c
Marcus van Bergen <mbhjvanbergen@gmail.com>
2018-09-27 10:01:09 +0200

    ECPCRM-170: rem Passy plaza and updated the modo script.

--- application/src/main/java/org/incode/eurocommercial/ecpcrm/module/application/fixture/scripts/import-views-modo.sql
+++ application/src/main/java/org/incode/eurocommercial/ecpcrm/module/application/fixture/scripts/import-views-modo.sql
@@ -8,25 +8,25 @@ SELECT
  "ef80ab638b" AS mailchimpListId,
  "accueil@modo-shopping.fr" AS contactEmail
 FROM `crm-import`.`Center` AS center
-WHERE code = 096;
+WHERE code = 095;
 
 CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `modo-import`.`User` AS
 SELECT *
 FROM `crm-import`.`User` AS user
-WHERE user.centerCode = 096;
+WHERE user.centerCode = 095;
 
 CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `modo-import`.`Card` AS
 SELECT *
 FROM `crm-import`.`Card` AS card
-WHERE card.centerCode = 096;
+WHERE card.centerCode = 095;
 
 CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `modo-import`.`Child` AS
 SELECT child.*
 FROM `crm-import`.`Child` AS child
 INNER JOIN `crm-import`.`User` AS parent ON child.parentReference = parent.reference
-WHERE parent.centerCode = 096;
+WHERE parent.centerCode = 095;
 
 CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `modo-import`.`CardRequest` AS
 SELECT *
 FROM `crm-import`.`CardRequest` AS cardRequest
-WHERE cardRequest.centerCode = 096;
+WHERE cardRequest.centerCode = 095;
